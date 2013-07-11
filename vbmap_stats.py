795b69fca7d4bffc47f43f57e83cf1b49e1ac0d8
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-07-10 17:01:42 -0700

    Plot some per tag histograms.

--- vbmap_stats.py
+++ vbmap_stats.py
@@ -24,8 +24,33 @@ def extract_nodes(vbmap):
 
     return sorted(result)
 
+def extract_tags(vbmap, nodes):
+    tags = vbmap['tags']
+    return dict((n, tags[i]) for i, n in enumerate(sorted(nodes)))
+
 def hist(chain, nodes):
     return [nodes[n] for n in chain]
+
+def tag_replication_counts(vbmap, nodes, tags_list, tags):
+    counts = []
+    for tag in tags_list:
+        tag_counts = []
+
+        for i, node in enumerate(nodes):
+            replicas = set()
+
+            for chain in vbmap:
+                if chain[0] != node:
+                    continue
+
+                replicas.update(r for r in chain[1:] if tags[r] == tag)
+
+            tag_counts.append(len(replicas))
+
+        counts.append(tag_counts)
+
+    return counts
+
 
 def main():
     vbmap = load_vbmap('vbmap.json')
@@ -38,6 +63,10 @@ def main():
 
     nodes_dict = dict((n, i) for i, n in enumerate(nodes))
 
+    tags = extract_tags(vbmap, nodes)
+    tags_list = sorted(set(tags.values()))
+    tags_count = len(tags_list)
+
     pylab.figure()
     pylab.subplot(211)
     pylab.xticks([i + 0.5 for i in xrange(nodes_count)], nodes)
@@ -47,7 +76,7 @@ def main():
     labels = ['master'] + ['replica %d' % i for i in xrange(len(replicas))]
 
     pylab.hist(plots, bins=xrange(nodes_count + 1), label=labels)
-    pylab.title("Histogram of number of vbuckets")
+    pylab.title("Number of vbuckets per node")
     pylab.xlabel("Nodes")
     pylab.ylabel("Number of vbuckets")
     pylab.legend()
@@ -58,9 +87,42 @@ def main():
     all_replicas = list(chain(*replicas))
     pylab.hist(hist(all_replicas, nodes_dict), bins=xrange(nodes_count + 1),
                label='all replicas', rwidth=0.5)
-    pylab.title("Histogram of number of replica vbuckets")
+    pylab.title("Number of replica vbuckets per node")
     pylab.xlabel("Nodes")
     pylab.ylabel("Number of vbuckets")
+    pylab.legend()
+
+    pylab.figure()
+    pylab.subplot(211)
+
+    plots = [[tags[n] for n in masters]] + \
+            [[tags[n] for n in r] for r in replicas]
+
+    pylab.hist(plots, bins=xrange(tags_count + 1), label=labels)
+
+    pylab.xticks([i + 0.5 for i in xrange(tags_count)], tags_list)
+    pylab.title("Number of vbuckets per tag")
+    pylab.xlabel("Tags")
+    pylab.ylabel("Number of vbuckets")
+    pylab.legend()
+
+    pylab.subplot(212)
+    pylab.xticks([i + 0.5 for i in xrange(nodes_count)], nodes)
+    pylab.title("Number of nodes each node replicates to per tag")
+    pylab.xlabel("Nodes")
+    pylab.ylabel("Number of replica nodes")
+
+    tags_repcounts = tag_replication_counts(vbmap['map'], nodes, tags_list, tags)
+
+    plots = []
+    for tag_counts in tags_repcounts:
+        plot = []
+
+        for node, count in enumerate(tag_counts):
+            plot.extend([node] * count)
+        plots.append(plot)
+
+    pylab.hist(plots, bins=xrange(nodes_count + 1), label=map(str, tags))
     pylab.legend()
 
     pylab.show()
